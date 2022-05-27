0763fd2b6fdbc661d22805427dd3f0c87179ddd3
Dongho Gang <gongdongho12@gmail.com>
2022-05-28 00:20:59 +0900

    feat: 줄 세우기 풀이 추가

--- src/2week/dongho/줄 세우기.kt
+++ src/2week/dongho/줄 세우기.kt
@@ -3,12 +3,42 @@ package `2week`.dongho
 import java.io.*
 import java.util.*
 
+// 이쪽 풀이 참고하시면 좋을것 같아요 설명 잘되어있음!
+// https://blog.naver.com/PostView.nhn?blogId=sweetgirl0111&logNo=222227941751&parentCategoryNo=&categoryNo=&viewDate=&isShowPopularPosts=false&from=postView
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
     val (N, M) = br.readLine().split(" ").map { it.toInt() }
 
-    bw.write("HelloWorld\n")
-    bw.flush()
-    bw.close()
+    val indegree = IntArray(N + 1)
+    val graph: MutableList<MutableList<Int>> = ArrayList()
+    for (i in 0 until N + 1) {
+        graph.add(ArrayList())
+    }
+    for (i in 0 until M) {
+        val (a, b) = br.readLine().split(" ").map { it.toInt() }
+        graph[a].add(b)
+        indegree[b]++
+    }
+
+    val q: Queue<Int> = LinkedList()
+    val result: Queue<Int> = LinkedList()
+    for (i in 1 until indegree.size) {
+        if (indegree[i] == 0) {
+            q.offer(i)
+        }
+    }
+
+    var current: Int
+    while (!q.isEmpty()) {
+        current = q.poll()
+        result.offer(current)
+        for (i in graph[current]) {
+            indegree[i]--
+            if (indegree[i] == 0) {
+                q.offer(i)
+            }
+        }
+    }
+
+    println(result.map { it.toString() }.joinToString(" "))
 }
