5dbcd2ea6052c7784b58fbb96a3ecc10029e0c36
soopeach <hsjeon584@gmail.com>
2022-05-27 15:27:24 +0900

    [Feat] : 가장 큰 수

--- src/1week/hyunsoo/부분수열의 합.kt
+++ src/1week/hyunsoo/부분수열의 합.kt
@@ -1,26 +1,26 @@
-val sizeAndCnt = readln().split(" ").map { it.toInt() }
-val num = readln().split(" ").map { it.toInt() }
-val result = mutableListOf<Int>()
-var ans = 0
-fun main() {
-
-    for (depth in 0 .. num.size){
-        combination(0, depth, 0)
-    }
-
-    if(sizeAndCnt[1] == 0) ans -= 1
-    println(ans)
-
-}
-
-fun combination(cnt : Int, depth: Int, beginwith : Int){
-    if (cnt == depth){
-        if (result.sum() == sizeAndCnt[1]) ans++
-        return
-    }
-    for (cur in beginwith until num.size){
-        result.add(num[cur])
-        combination(cnt+1, depth, cur +  1)
-        result.removeAt(result.lastIndex)
-    }
-}
+//val sizeAndCnt = readln().split(" ").map { it.toInt() }
+//val num = readln().split(" ").map { it.toInt() }
+//val result = mutableListOf<Int>()
+//var ans = 0
+//fun main() {
+//
+//    for (depth in 0 .. num.size){
+//        combination(0, depth, 0)
+//    }
+//
+//    if(sizeAndCnt[1] == 0) ans -= 1
+//    println(ans)
+//
+//}
+//
+//fun combination(cnt : Int, depth: Int, beginwith : Int){
+//    if (cnt == depth){
+//        if (result.sum() == sizeAndCnt[1]) ans++
+//        return
+//    }
+//    for (cur in beginwith until num.size){
+//        result.add(num[cur])
+//        combination(cnt+1, depth, cur +  1)
+//        result.removeAt(result.lastIndex)
+//    }
+//}
--- src/1week/hyunsoo/차이를 최대로.kt
+++ src/1week/hyunsoo/차이를 최대로.kt
@@ -1,40 +1,40 @@
-import kotlin.math.abs
-
-val size = readln().toInt()
-val num = readln().split(" ").map { it.toInt() }
-val visited = BooleanArray(num.size, { false })
-val result = mutableListOf<Int>()
-var ans = 0
-fun main() {
-
-    permutation(0, size)
-    println(ans)
-
-}
-
-// 순열로 모든 경우의 수 확인
-fun permutation(cnt : Int, depth : Int) {
-    if (cnt == depth) {
-        // 이전 순열의 최대값(ans)보다 현재 순열의 최대값이 더 크다면 갱신
-        if (getMax(result) > ans) ans = getMax(result)
-        return
-    }
-    for (i in 0 until num.size) {
-        if (visited[i] == false) {
-            visited[i] = true
-            result.add(num[i])
-            permutation(cnt + 1, depth)
-            result.removeAt(result.lastIndex)
-            visited[i] = false
-
-        }
-    }
-}
-// 수식을 계산
-fun getMax(array: MutableList<Int>): Int {
-    var ans = 0
-    for (index in 2..array.size) {
-        ans += abs(array[index - 2] - array[index - 1])
-    }
-    return ans
-}
+//import kotlin.math.abs
+//
+//val size = readln().toInt()
+//val num = readln().split(" ").map { it.toInt() }
+//val visited = BooleanArray(num.size, { false })
+//val result = mutableListOf<Int>()
+//var ans = 0
+//fun main() {
+//
+//    permutation(0, size)
+//    println(ans)
+//
+//}
+//
+//// 순열로 모든 경우의 수 확인
+//fun permutation(cnt : Int, depth : Int) {
+//    if (cnt == depth) {
+//        // 이전 순열의 최대값(ans)보다 현재 순열의 최대값이 더 크다면 갱신
+//        if (getMax(result) > ans) ans = getMax(result)
+//        return
+//    }
+//    for (i in 0 until num.size) {
+//        if (visited[i] == false) {
+//            visited[i] = true
+//            result.add(num[i])
+//            permutation(cnt + 1, depth)
+//            result.removeAt(result.lastIndex)
+//            visited[i] = false
+//
+//        }
+//    }
+//}
+//// 수식을 계산
+//fun getMax(array: MutableList<Int>): Int {
+//    var ans = 0
+//    for (index in 2..array.size) {
+//        ans += abs(array[index - 2] - array[index - 1])
+//    }
+//    return ans
+//}
