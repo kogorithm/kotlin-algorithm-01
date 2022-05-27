b51b8beed6eff497fe01a9938801531994580ed5
soopeach <hsjeon584@gmail.com>
2022-05-27 22:36:29 +0900

    [Feat] 줄 세우기 추가

--- src/2week/hyunsoo/줄 세우기.kt
+++ src/2week/hyunsoo/줄 세우기.kt
@@ -3,31 +3,30 @@ package `2week`.hyunsoo
 import java.util.LinkedList
 import java.util.Queue
 
-fun main(){
+fun main() {
     val (N, M) = readln().split(" ").map { it.toInt() }
 
-    var indegree = IntArray(N+1, { 0 })
-    val graph = Array<MutableList<Int>>(N+1, { mutableListOf() })
+    var indegree = IntArray(N + 1, { 0 })
+    val graph = Array<MutableList<Int>>(N + 1, { mutableListOf() })
     val result = mutableListOf<Int>()
-    var queue : Queue<Int> = LinkedList<Int>()
+    var queue: Queue<Int> = LinkedList<Int>()
 
-    repeat(M){
+    repeat(M) {
         val (a, b) = readln().split(" ").map { it.toInt() }
         graph[a].add(b)
         indegree[b] += 1
     }
 
-    for (index in 1 .. N){
-        if (indegree[index] == 0)
-            queue.add(index)
+    for (i in 1..N) {
+        if (indegree[i] == 0)
+            queue.add(i)
     }
 
-    while (queue.isNotEmpty()){
+    while (queue.isNotEmpty()) {
 
-        val cur = queue.peek()
-        queue.poll()
+        val cur = queue.poll()
         result.add(cur)
-        for (index in graph[cur]){
+        for (index in graph[cur]) {
             indegree[index] -= 1
             if (indegree[index] == 0)
                 queue.add(index)
