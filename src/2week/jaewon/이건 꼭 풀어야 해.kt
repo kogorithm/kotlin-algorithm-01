0d1fadf360324c9ee538274c6d7506f99a8d6a55
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-05-28 04:39:55 +0900

    [Feat] 이건 꼭 풀어야 해

---
+++ src/2week/jaewon/이건 꼭 풀어야 해.kt
@@ -0,0 +1,25 @@
+package `2week`.jaewon
+
+import java.io.*
+
+fun main()  {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val(n,q) = br.readLine().split(" ").map { it.toInt()}
+    val numbers = br.readLine().split(" ").map { it.toInt()}.sorted()
+    val sumList = Array(n+1) { 0 }
+
+    numbers.forEachIndexed { index, i ->
+        sumList[index+1] = sumList[index]+i
+    }
+
+    repeat(q){
+        val quiz = br.readLine().split(" ").map { it.toInt()}
+        val answer = sumList[quiz[1]]-sumList[quiz[0]-1]
+
+        bw.write("$answer\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
