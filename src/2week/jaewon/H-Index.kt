4ea534d243049536e69f7e802d19197149c13d3b
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-05-28 14:11:35 +0900

    [Refactor] 코드 수정

--- src/2week/jaewon/H-Index.kt
+++ src/2week/jaewon/H-Index.kt
@@ -1,17 +1,30 @@
 package `2week`.jaewon
 
-fun main(){
-    println(hIndex(intArrayOf(10,10,10,10,10)))
-    println(hIndex(intArrayOf(3,0,6,1,5)))
-    println(hIndex(intArrayOf(0,0,0,0,0)))
-}
 
-fun hIndex(citations: IntArray): Int {
-    var answer = 0
-    citations.sortedDescending().forEachIndexed{ index, num ->
-        if( index+1 <= num ){ // 인용된 논문의 횟수보다 인용되지 않은 논문의 횟수가 더 많아지는 지점.
-            answer = index+1
+class `H-index` {
+    companion object {
+        fun getSolution(): Solution {
+            return Solution()
         }
     }
-    return answer
+
+    class Solution {
+        fun solution(citations: IntArray): Int {
+            var answer = 0
+            citations.sortedDescending().forEachIndexed{ index, num ->
+                if( index+1 <= num ){ // 인용된 논문의 횟수보다 인용되지 않은 논문의 횟수가 더 많아지는 지점
+                    answer = index+1 // [0]6,[1]5,[2]3,[3]1,[4]0
+                }
+            }
+            return answer
+        }
+    }
+}
+
+
+fun main(){
+    val hIndex = `H-index`.getSolution()
+    println(hIndex.solution(intArrayOf(10,10,10,10,10)))
+    println(hIndex.solution(intArrayOf(3,0,6,1,5)))
+    println(hIndex.solution(intArrayOf(0,0,0,0,0)))
 }
--- src/2week/jaewon/가장 큰 수.kt
+++ src/2week/jaewon/가장 큰 수.kt
@@ -1,19 +1,30 @@
 package `2week`.jaewon
 
+
+class `가장 큰 수` {
+    companion object {
+        fun getSolution(): Solution {
+            return Solution()
+        }
+    }
+
+    class Solution {
+        fun solution(numbers: IntArray): String {
+            var answer = ""
+            numbers.sortedWith(Comparator { a, b ->
+                val aString = a.toString()
+                val bString = b.toString()
+                (bString+aString).compareTo(aString+bString)
+            }).forEach { answer += it.toString()}
+
+            if(answer[0] == '0') answer ="0"
+
+            return answer
+        }
+    }
+}
+
 fun main(){
-    println(solution(intArrayOf(0,0,0,70)))
-
+    println(`가장 큰 수`.getSolution().solution(intArrayOf(0,0,0,70)))
 }
 
-fun solution(numbers: IntArray): String {
-    var answer = ""
-    numbers.sortedWith(Comparator { a, b ->
-        val aString = a.toString()
-        val bString = b.toString()
-        (bString+aString).compareTo(aString+bString)
-    }).forEach { answer += it.toString() }
-
-    if(answer[0] == '0') answer ="0"
-
-    return answer
-}
