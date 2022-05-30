f7cc10126c65b720ba61bc0c84625400cdac1c7d
soopeach <hsjeon584@gmail.com>
2022-05-30 11:34:42 +0900

    Feat: 서로 다른 부분 문자열의 개수 추가

--- src/3week/hyunsoo/서로 다른 부분 문자열의 개수.kt
+++ src/3week/hyunsoo/서로 다른 부분 문자열의 개수.kt
@@ -1,36 +1,19 @@
 package `3week`.hyunsoo
 
-class `서로 다른 부분 문자열의 개수` {
-    companion object {
-        fun getThis() = `서로 다른 부분 문자열의 개수`()
-    }
+fun main(){
 
-    var s = readln().toList()
-    val tempList = mutableListOf<Char>()
-    val answerList = mutableSetOf<String>()
+    val s = readln()
+    val substringSet = mutableSetOf<String>()
 
-    fun combination(cnt : Int, depth : Int, beginWith : Int){
-        if (cnt == depth){
-            val tempString = tempList.toString()
-            answerList.add(tempString)
+    for (start in 0 .. s.length){
+
+        for (end in start+1 .. s.length){
+
+            substringSet.add(s.substring(start,end))
+
         }
-        for (cur in beginWith until  s.size){
-            tempList.add(s[cur])
-            combination(cnt+1,depth,cur+1)
-            tempList.removeAt(tempList.lastIndex)
-        }
-
     }
-
-}
-fun main() {
-    val temp = `서로 다른 부분 문자열의 개수`.getThis()
-
-    for (no in 1 until temp.s.size){
-       temp.combination(no,temp.s.size,0)
-    }
-    print(temp.answerList.size)
-
+    println(substringSet.size)
 }
 
 
