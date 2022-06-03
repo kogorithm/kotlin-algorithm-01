8ae72ce3a7cbf3599e1ed94994daeeaa7f433d21
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-03 23:42:39 +0900

    [Feat] 문자열 잘라내기

---
+++ src/week3/jaewon/문자열 잘라내기.kt
@@ -0,0 +1,20 @@
+package week3.jaewon
+
+fun main(){
+    val (r,c) = readln().split(" ").map{it.toInt()}
+    val strings = Array(r){readln()}.toMutableList()
+    var count = 0
+
+    while (strings.size != 0){
+        val tempSet = mutableSetOf<String>()
+        for (i in 0 until strings[0].length){
+            val temp = strings.map{it[i]}.joinToString("")
+            tempSet.add(temp)
+        }
+        if(tempSet.size == c) {
+            strings.removeAt(0)
+            count++
+        }else break
+    }
+    println(count-1)
+}
