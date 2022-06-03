7a2679f30c3580656aad895444509bed3b8924a9
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-04 02:31:42 +0900

    [Feat] 문자열 잘라내기 다시 풀기..
    성공~~~

--- src/3week/jaewon/문자열 잘라내기.kt
+++ src/3week/jaewon/문자열 잘라내기.kt
@@ -2,19 +2,22 @@ package `3week`.jaewon
 
 fun main(){
     val (r,c) = readln().split(" ").map{it.toInt()}
-    val strings = Array(r){readln()}.toMutableList()
+    val strings = MutableList(r){readln()}
     var count = 0
+    val setList = mutableListOf<String>()
 
-    while (strings.size != 0){
+    for (i in 0 until strings[0].length){
+        val temp = strings.map{it[i]}.joinToString("")
+        setList.add(temp)
+    }
+
+    for (i in 1 ..r){
         val tempSet = mutableSetOf<String>()
-        for (i in 0 until strings[0].length){
-            val temp = strings.map{it[i]}.joinToString("")
+        setList.forEach {
+            val temp = it.substring(i-1,r)
             tempSet.add(temp)
         }
-        if(tempSet.size == c) {
-            strings.removeAt(0)
-            count++
-        }else break
+        if(tempSet.size == c) {count++} else break
     }
     println(count-1)
 }
