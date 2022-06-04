5d16b49fbe80c3a6a432d5be2a4f9d3df95bc758
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-04 21:10:45 +0900

    [Refactor] 코드 정리

--- src/3week/jaewon/신고 결과 받기.kt
+++ src/3week/jaewon/신고 결과 받기.kt
@@ -8,17 +8,18 @@ class `신고 결과 받기` {
     }
     class Solution {
         fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
-            val noOverLap = ((report.groupBy{it.split(" ")[1]}).values.map{it.distinct()})
             val temp = id_list.associateWith { 0 }.toMutableMap()
 
-            noOverLap.forEach {
-                if (it.size >= k) {
-                    val reporters = it.map {reporter -> reporter.split(" ")[0]}
-                    reporters.forEach { reporter ->
-                        temp[reporter] = temp[reporter]!! + 1
-                    }
-                }
+            report.groupBy { it.split(" ")[1]}
+                .values
+                .map { it.distinct() }
+                .filter { it.size>= k }
+                .map{ it.map { reporter -> reporter.split(" ")[0]} }
+                .flatten()
+                .forEach {  reporter ->
+                temp[reporter] = temp[reporter]!! + 1
             }
+
             return temp.values.toIntArray()
         }
     }
