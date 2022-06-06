ceeef08a841e2ac3c557fb3740f4934a0b765738
soopeach <hsjeon584@gmail.com>
2022-06-06 17:34:39 +0900

    Feat: 촌수계산 추가

--- src/4week/hyunsoo/바이러스.kt
+++ src/4week/hyunsoo/바이러스.kt
@@ -1,36 +1,39 @@
 package `4week`.hyunsoo
 
 
-lateinit var visited: BooleanArray
-lateinit var netWork: Array<MutableList<Int>>
-var ans = 0
-fun main() {
+class `바이러스`(){
+    lateinit var visited: BooleanArray
+    lateinit var netWork: Array<MutableList<Int>>
+    var ans = 0
+    fun main() {
 
-    // 컴푸터의 수
-    val cntOfCom = readln().toInt()
-    // 컴퓨터 쌍의 수
-    val cntOfLine = readln().toInt()
+        // 컴푸터의 수
+        val cntOfCom = readln().toInt()
+        // 컴퓨터 쌍의 수
+        val cntOfLine = readln().toInt()
 
-    netWork = Array(cntOfCom + 1, { mutableListOf() })
-    visited = BooleanArray(cntOfCom + 1, { false })
+        netWork = Array(cntOfCom + 1, { mutableListOf() })
+        visited = BooleanArray(cntOfCom + 1, { false })
 
-    repeat(cntOfLine) {
-        val (a, b) = readln().split(" ").map { it.toInt() }
-        netWork[a].add(b)
-        netWork[b].add(a)
+        repeat(cntOfLine) {
+            val (a, b) = readln().split(" ").map { it.toInt() }
+            netWork[a].add(b)
+            netWork[b].add(a)
+        }
+
+        dfs(1)
+        println(ans)
+
     }
 
-    dfs(1)
-    println(ans)
-
-}
-
-fun dfs(start: Int) {
-    for (connectedCom in netWork[start]) {
+    fun dfs(start: Int) {
         visited[start] = true
-        if (visited[connectedCom] == false) {
-            ans += 1
-            dfs(connectedCom)
+        for (connectedCom in netWork[start]) {
+            if (visited[connectedCom] == false) {
+                visited[connectedCom] == true
+                ans += 1
+                dfs(connectedCom)
+            }
         }
     }
 }
