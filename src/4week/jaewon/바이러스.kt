9b67136fbfebfd3aa78299cbaf3370a26141e632
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-12 04:01:14 +0900

    [Feat] 바이러스

---
+++ src/4week/jaewon/바이러스.kt
@@ -0,0 +1,35 @@
+package `4week`.jaewon
+
+fun main(){
+    val n = readln().toInt() //정점의 갯수
+    val e = readln().toInt() // 간선의 갯수
+    val graph = Array(n+1){ mutableListOf<Int>()}
+    repeat(e){
+        val (a,b) = readln().split(" ").map{ it.toInt()}
+        graph[a].add(b)
+        graph[b].add(a)
+    }
+
+    dfs(graph,n)
+}
+
+fun dfs(graph : Array<MutableList<Int>>,size : Int){
+    graph.forEach { it.sortDescending()}
+    val visited = Array(size+1){ false }
+    val stack = mutableListOf<Int>()
+
+    stack.add(1)
+    visited[1] = true
+    while (stack.isNotEmpty()){
+        val top = stack.removeLast() //pop
+        if (!visited[top]){ // 현재 top이 방문했는지 확인
+            visited[top] = true
+        }
+        graph[top].forEach { //현재 top과 인접한 얘들 반복
+            if (!visited[it]) {
+                stack.add(it)
+            }
+        }
+    }
+    println(visited.filter{it}.size-1)
+}
