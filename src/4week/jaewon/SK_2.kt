caa580fbce863c45c6afe9d4a7ebdbe9f2a8bade
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-12 21:40:29 +0900

    [Feat] 2번 문제

---
+++ src/4week/jaewon/SK_2.kt
@@ -0,0 +1,52 @@
+package `4week`.jaewon
+
+fun solution(periods: IntArray, payments: Array<IntArray>, estimates: IntArray): IntArray {
+    val answer= MutableList(2){0}
+    val isVIP = MutableList(payments.size-1){false}
+
+    periods.zip(payments).forEachIndexed { index, pair ->
+        when(pair.first){
+            in 0 until 24 -> { }
+            in 24 until 60 -> {
+                if(pair.second.sum() >= 900000){
+                    isVIP.add(index,true)
+                }
+            }
+            else -> {
+                if(pair.second.sum() >= 600000){
+                    isVIP.add(index,true)
+                }
+            }
+        }
+    }
+
+    periods.zip(payments).forEachIndexed{index, pair ->
+        val newSum = pair.second.drop(1).sum() + estimates[index]
+        when(pair.first+1){
+            in 0 until 24 -> { }
+            in 24 until 60 -> {
+                if(isVIP[index] && newSum < 900000){
+                    // 이전달에 VIP였지만 이번달은 VIP가 아닌경우
+                    answer[1]++
+                }
+                if(!isVIP[index] && newSum >= 900000){
+                    answer[0]++
+                }
+            }
+            else -> {
+                if(isVIP[index] && newSum < 600000){
+                    // 이전달에 VIP였지만 이번달은 VIP가 아닌경우
+                    answer[1]++
+                }
+                if(!isVIP[index] && newSum >= 600000){
+                    answer[0]++
+                }
+            }
+        }
+    }
+    return answer.toIntArray()
+}
+
+fun main(){
+
+}
