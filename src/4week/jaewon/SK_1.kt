1c6fbbdeac2105840834c23de5711e4e77566fb6
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-12 22:39:47 +0900

    [Refactor] 중복제거를 위해 함수 생성, class 생성

--- src/4week/jaewon/SK_1.kt
+++ src/4week/jaewon/SK_1.kt
@@ -1,22 +1,32 @@
 package `4week`.jaewon
 
-fun solution(p: IntArray): IntArray {
-    val answer = IntArray(p.size){0}
-    var i = 0
-    while(i < p.size){
-        val j = p.indexOf(p.drop(i).minOf{it})
-        if(i != j){
-            val temp = p[i]
-            p[i] = p[j]
-            p[j] = temp
-            answer[i]++
-            answer[j]++
-        }
-        i++
+class SK_1 {
+    fun getSolution(): Solution {
+        return Solution()
     }
-    return answer
+
+    class Solution{
+        fun solution(p: IntArray): IntArray {
+        val answer = IntArray(p.size){0}
+        var i = 0
+        while(i < p.size){
+            val j = p.indexOf(p.drop(i).minOf{it})
+            if(i != j){
+                val temp = p[i]
+                p[i] = p[j]
+                p[j] = temp
+                answer[i]++
+                answer[j]++
+            }
+            i++
+        }
+
+        return answer
+    }
+    }
 }
 
 fun main(){
-    println(solution(intArrayOf(2, 5, 3, 1, 4)))
+    val solution = SK_1().getSolution()
+    println(solution.solution(intArrayOf(2, 5, 3, 1, 4)))
 }
