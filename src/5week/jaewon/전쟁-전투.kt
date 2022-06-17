526191e9f1eb30a6f672125eb44f621106017fa7
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-06-18 01:04:47 +0900

    [Feat] 전쟁-전투

---
+++ src/5week/jaewon/전쟁-전투.kt
@@ -0,0 +1,65 @@
+package `5week`.jaewon
+
+fun main(){
+    var answer = Array(2){0}
+    val (n,m) = readln().split(" ").map{it.toInt()}
+    val matrix = Array(m){ mutableListOf<Int>()}
+    val visited = Array(m){ Array(n){false}}
+    repeat(m){index ->
+        val list = readln().map{ if( it == 'W' ) 1 else 0 }.toMutableList()
+        matrix[index] = list
+    }
+
+    //아군
+    matrix.forEachIndexed { i, it ->
+        it.forEachIndexed{ j, _ ->
+            if(!visited[i][j] && matrix[i][j] == 1){
+                visited[i][j] = true
+                val temp = findOurs(matrix,visited,i,j,1)
+                answer[0] += temp*temp
+            }
+        }
+    }
+
+    //적군
+    matrix.forEachIndexed { i, it ->
+        it.forEachIndexed{ j, _ ->
+            if(!visited[i][j] && matrix[i][j] == 0){
+                visited[i][j] = true
+                val temp = findOurs(matrix,visited,i,j,0)
+                answer[1] += temp*temp
+            }
+        }
+    }
+
+    println("${answer[0]} ${answer[1]}")
+}
+
+fun findOurs(matrix : Array<MutableList<Int>>, visited : Array<Array<Boolean>> ,a : Int,b : Int, type : Int): Int {
+    var cnt = 0
+    val queue = mutableListOf<Pair<Int,Int>>()
+    val dx = intArrayOf(0,1,0,-1)
+    val dy = intArrayOf(1,0,-1,0)
+
+    queue.add(Pair(a,b)) // 초기위치 큐에 넣기
+
+    while(queue.isNotEmpty()){
+        repeat(queue.size){
+            val front = queue.removeLast()
+            val x = front.first
+            val y = front.second
+            repeat(4){time ->
+                val nx = x+dx[time]
+                val ny = y+dy[time]
+                if(nx in matrix.indices && ny in 0 until matrix[0].size && matrix[nx][ny] == type){
+                    if(!visited[nx][ny]){
+                        visited[nx][ny] = true
+                        queue.add(Pair(nx,ny))
+                        cnt++
+                    }
+                }
+            }
+        }
+    }
+    return cnt+1
+}
