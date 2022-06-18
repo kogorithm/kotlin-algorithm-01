cde47019fd333446528a3b2a0bf6cb227860c95c
sypark <seoyoon528@gmail.com>
2022-06-18 12:38:56 +0900

    [Fix] 코드 수정

--- src/5week/seoyoon/타겟 넘버.kt
+++ src/5week/seoyoon/타겟 넘버.kt
@@ -12,21 +12,21 @@ class `타겟 넘버` {
     class Solution {
         fun solution(numbers: IntArray, target: Int): Int {
             answer = 0
-            BFS(numbers, target, 0, 0, 0)
+            DFS(numbers, target, 0, 0, 0)
             return answer
         }
     }
 }
 
-fun BFS(numbers: IntArray, target: Int, depth: Int, sum: Int, idx: Int) {
+fun DFS(numbers: IntArray, target: Int, depth: Int, sum: Int, idx: Int) {
     if (depth == numbers.size) {
         if (sum == target) {
             answer++
         }
         return
     }
-    BFS(numbers, target, depth + 1, sum + numbers[idx], idx + 1)
-    BFS(numbers, target, depth + 1, sum - numbers[idx], idx + 1)
+    DFS(numbers, target, depth + 1, sum + numbers[idx], idx + 1)
+    DFS(numbers, target, depth + 1, sum - numbers[idx], idx + 1)
 }
 
 fun main() {
