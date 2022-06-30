5139d5a2d05914a4684a593f9e6a42bde0877ee7
sypark <seoyoon528@gmail.com>
2022-06-30 13:45:37 +0900

    [Refactor] 프린터 큐 주석 제거

--- src/7week/seoyoon/프린터 큐.kt
+++ src/7week/seoyoon/프린터 큐.kt
@@ -6,20 +6,15 @@ fun main() {
     val t = readln().toInt()
     var queue : Queue<Pair<Int, Int>>
     var sortedOrder : Queue<Int>
-    // 1 1 9 1 1 1
-    // 5 6 1 2 3 4
+
     repeat(t) {
         val (n, m) = readln().split(" ").map{ it.toInt() }
-        // 1 1 9 1 1 1
         val value = readln().split(" ").map{ it.toInt() }
         var cnt = 1
 
-        // 9 1 1 1 1 1
         sortedOrder = LinkedList(value.sortedDescending())
         queue = LinkedList()
         value.forEachIndexed { index, it ->
-            // 0 1 2 3 4 5
-            // 1 1 9 1 1 1
             queue.add(Pair(index, it))
         }
 
