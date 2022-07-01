6844b2251af9021d8566926da5e9a9e28a6b8929
Teddy <teddy.ryan@kakaostyle.com>
2022-07-02 03:31:58 +0900

    fixed

--- src/7week/dongho/크리스마스 선물.kt
+++ src/7week/dongho/크리스마스 선물.kt
@@ -7,19 +7,19 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     var st: StringTokenizer
     val N = br.readLine().toInt()
-    val q = PriorityQueue(Collections.reverseOrder<Int>())
+    val pq = PriorityQueue(Collections.reverseOrder<Int>())
     repeat(N) {
         st = StringTokenizer(br.readLine())
         val a = st.nextToken().toInt()
         if (a == 0) {
-            if (q.isEmpty()) {
+            if (pq.isEmpty()) {
                 println(-1)
             } else {
-                println(q.poll())
+                println(pq.poll())
             }
         } else {
             repeat(a) {
-                q.offer(st.nextToken().toInt())
+                pq.offer(st.nextToken().toInt())
             }
         }
     }
