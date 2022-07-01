13114cf201d9457216ad162bcd5ddca4f0360e06
sypark <seoyoon528@gmail.com>
2022-07-01 22:59:13 +0900

    [Feat] 스택 수열

---
+++ src/7week/seoyoon/스택 수열.kt
@@ -0,0 +1,35 @@
+package `7week`.seoyoon
+
+import java.util.*
+
+fun main() {
+    var n = readln().toInt()
+    var seq = IntArray(n)
+    var stack = Stack<Int>()
+    var answer = mutableListOf<Char>()
+    var top = 0
+
+    repeat(n) {
+        seq[it] = readln().toInt()
+    }
+
+    for (i in 1 .. n) {
+        stack.push(i)
+        answer.add('+')
+
+        while (stack.isNotEmpty() && stack.peek() == seq[top]) {
+            stack.pop()
+            answer.add('-')
+            top++
+        }
+    }
+
+    if (stack.isNotEmpty()) {
+        println("NO")
+    }
+    else {
+        answer.forEach {
+            println(it)
+        }
+    }
+}
