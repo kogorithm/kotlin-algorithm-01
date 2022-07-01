9aca7eea6291c02091bced6729f045b8ff3e6d3e
sypark <seoyoon528@gmail.com>
2022-07-01 15:12:08 +0900

    [Feat] 요세푸스 문제

---
+++ src/7week/seoyoon/요세푸스 문제.kt
@@ -0,0 +1,33 @@
+package `7week`.seoyoon
+
+import java.util.LinkedList
+
+fun main() {
+    // 3 6 2 7 5 1 4
+    val (n, k) = readln().split(" ").map{ it.toInt() }
+    var queue = LinkedList<Int>()
+    var answer = LinkedList<Int>()
+
+    (1 .. n).forEach {
+        queue.add(it)
+    }
+    
+    var idx = 1
+    while (queue.isNotEmpty()) {
+        if (idx == k) {
+            answer.add(queue.poll())
+            idx = 1
+        }
+        else {
+            var tmp = queue.poll()
+            queue.add(tmp)
+            idx++
+        }
+    }
+
+    print("<")
+    for (i in 0 until answer.size-1) {
+        print("${answer.poll()}, ")
+    }
+    print("${answer.poll()}>")
+}
