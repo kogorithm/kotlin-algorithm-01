3ffb0e7cdfbf1d4d042c6cf82053177a2c1e68a2
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-07-15 23:52:40 +0900

    [Feat] 1번 문제

---
+++ src/8week/jaewon/스킬체크1.kt
@@ -0,0 +1,17 @@
+
+class `스킬체크1` {
+    companion object {
+        fun getSolution(): Solution {
+            return Solution()
+        }
+
+        class Solution {
+            fun solution(seoul: Array<String>): String =
+                "김서방은 ${seoul.indexOf("Kim")}에 있다"
+        }
+    }
+}
+
+
+
+
